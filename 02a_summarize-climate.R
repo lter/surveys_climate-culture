62cc1701854802bf79db5dadcb31729ed9fe375b
njlyon0 <nicholasjlyon@gmail.com>
2025-08-06 09:36:43 -0400

    feat (summarize): more pre-summarization prep for climate data

--- 02a_summarize-climate.R
+++ 02a_summarize-climate.R
@@ -14,7 +14,7 @@
 ## ----------------------------- ##
 
 # Load libraries
-librarian::shelf(tidyverse)
+librarian::shelf(tidyverse, supportR)
 
 # Clear environment
 rm(list = ls()); gc()
@@ -33,13 +33,81 @@ clim_v1 <- read.csv(file = file.path("data", "01_processed-climate.csv")) %>%
 # Check structure
 dplyr::glimpse(clim_v1)
 
+## ----------------------------- ##
+# Remove Unwanted Columns ----
+## ----------------------------- ##
+
+# Remove unwanted columns
+clim_v2 <- clim_v1 %>% 
+  # Don't want free text columns at this point
+  dplyr::select(-dplyr::ends_with(c("_other", "_text"))) %>% 
+  dplyr::select(-dplyr::starts_with(c("open_question_"))) %>% 
+  # Remove site-specific columns too
+  dplyr::select(-dplyr::starts_with(c("gce_", "kbs_", "luq_"))) %>% 
+  # Original respondent activities column in superseded
+  dplyr::select(-respondent_activities)
+
+# What was lost?
+supportR::diff_check(old = names(clim_v1), new = names(clim_v2))
+
+# Check structure
+dplyr::glimpse(clim_v2)
+
+## ----------------------------- ##
+# Check Sites ----
+## ----------------------------- ##
+
+# What sites are in the data?
+sort(unique(clim_v2$site))
+
+# Want to extract site abbreviations (for graphs)
+clim_v3 <- clim_v2 %>% 
+  # Resolve "Other" entry (just one response had this)
+  dplyr::mutate(site = ifelse(site == "Other",
+                              # Response in 'site other' column: "pie and gce"
+                              yes = "Plum Island Ecosystems LTER (PIE)", 
+                              no = site)) %>% 
+  # Split into separate columns by parentheses
+  tidyr::separate_wider_delim(cols = site, delim = " (",
+                              names = c("site_name", "site")) %>% 
+  # Remove trailing parentheses left over
+  dplyr::mutate(site = gsub(pattern = "\\)", replacement = "", x = site))
+
+# What sites are left?
+sort(unique(clim_v3$site))
+
+# Check structure
+dplyr::glimpse(clim_v3)
+
+## ----------------------------- ##
+# Fill Missing Values ----
+## ----------------------------- ##
+
+# Need to fill missing values in some columns
+clim_v4 <- clim_v3 %>% 
+  # Antagonistic interactions
+  dplyr::mutate(antagonistic_interactions_stage = ifelse(is.na(antagonistic_interactions_stage),
+                                                         yes = "No antagonistic interactions",
+                                                         no = antagonistic_interactions_stage))
+
+# Check structure
+dplyr::glimpse(clim_v4)
+
+
+
+
+
+
+
+
+
 
 ## ----------------------------- ##
 # Export ----
 ## ----------------------------- ##
 
 # Make a final object
-clim_v99 <- clim_v1
+clim_v99 <- clim_v2
 
 # Export locally
 write.csv(x = clim_v99, row.names = F, na = '',
