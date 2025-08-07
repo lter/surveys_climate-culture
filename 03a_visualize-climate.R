b6e4041331c42f0c363f6cc425dd8063f55e9138
njlyon0 <nicholasjlyon@gmail.com>
2025-08-07 14:23:30 -0400

    feat (viz): working on another variant of graph for comparing network-wide responses to those of a particular site

--- 03a_visualize-climate.R
+++ 03a_visualize-climate.R
@@ -36,6 +36,27 @@ res_v1 <- read.csv(file = file.path("data", "02a_summarized-climate.csv"))
 dplyr::glimpse(res_v1)
 
 ## ----------------------------- ##
+# Process Data ----
+## ----------------------------- ##
+
+# We also want a different format of the results data for some plots
+res_net <- res_v1 %>% 
+  dplyr::select(-site, -total:-percent) %>% 
+  dplyr::mutate(site = "Network") %>% 
+  dplyr::rename_with(.fn = ~ gsub(pattern = "network_", replacement = "", x = .)) %>% 
+  dplyr::distinct()
+
+# Check structure
+dplyr::glimpse(res_net)
+
+# Combine with other result data
+res_v2 <- dplyr::bind_rows(res_v1, res_net)
+
+# Check that out
+sort(unique(res_v2$site))
+dplyr::glimpse(res_v2)
+
+## ----------------------------- ##
 # Activities ----
 ## ----------------------------- ##
 
@@ -55,8 +76,8 @@ ord <- c("Education or public engagement" = "#fca311",
          "Administrative duties" = "#dad7cd")
 
 # Prepare data
-df_prep <- res_v1 %>% 
-  dplyr::filter(question == "respondent_activities") %>% 
+df_prep <- res_v2 %>% 
+  dplyr::filter(question == "respondent_activities" & !is.na(network_percent)) %>% 
   dplyr::mutate(answer = factor(answer, levels = names(ord)))
 
 # Check structure
@@ -116,7 +137,87 @@ for(focal_site in sort(unique(df_prep$site))){
 } # Close loop
 
 # Clear environment
-rm(list = c("ord", "df_prep", "df_net"))
+rm(list = c("ord", "df_prep", "df_net", "focal_site"))
+
+## ----------------------------- ##
+# Intense Data Collection ----
+## ----------------------------- ##
+
+# Identify preferred order & colors
+ord <- c("Other" = "gray80",
+         "0 weeks" = "#e9d8a6",
+         "1-3 weeks" = "#ee9b00",
+         "1-3 months" = "#bb3e03",
+         "Longer" = "#540b0e")
+
+# Prepare data
+df_prep <- res_v2 %>% 
+  dplyr::filter(question == "fieldwork_duration") %>% 
+  dplyr::mutate(answer = ifelse(answer == "I don't participate in this type of data collection",
+                                yes = "0 weeks", no = answer)) %>% 
+  dplyr::mutate(answer = factor(answer, levels = rev(names(ord)))) %>% 
+  dplyr::mutate(site = factor(site, levels = c("Network", setdiff(sort(site), "Network"))))
+
+# Check structure
+dplyr::glimpse(df_prep)
+
+# Loop across sites
+for(focal_site in setdiff(sort(unique(df_prep$site)), "Network")){
+  
+  # Progress message
+  message("Making graph for ", focal_site)
+  
+  # Make graph
+  plot <- ggplot(dplyr::filter(df_prep, site %in% c("Network", focal_site)), 
+                      aes(x = site, y = percent, fill = answer, color = "x")) +
+    geom_bar(stat = "identity") +
+    scale_color_manual(values = "#000") +
+    scale_fill_manual(values = ord) +
+    labs(y = "Percent Responses") +
+    guides(color = "none") +
+    theme_bw() +
+    theme(legend.position = "right",
+          legend.title = element_blank(),
+          axis.title.x = element_blank(),
+          axis.title.y = element_text(size = 12),
+          axis.text.x = element_text(size = 12),
+          axis.text.y = element_text(size = 10)); plot
+  
+  # Export locally
+  ggsave(filename = file.path("graphs", paste0("fieldwork_duration_", focal_site, ".png")),
+         height = 6, width = 6, units = "in")
+  
+} # Close loop
+
+# Clear environment
+rm(list = c("ord", "df_prep", "focal_site", "plot"))
+
+
+## ----------------------------- ##
+# Template ----
+## ----------------------------- ##
+
+# Identify preferred order & colors
+ord <- c("" = "#")
+
+# Prepare data
+df_prep <- res_v1 %>% 
+  dplyr::filter(question == "respondent_activities") %>% 
+  dplyr::mutate(answer = factor(answer, levels = names(ord)))
+
+# Check structure
+dplyr::glimpse(df_prep)
+
+# Make a 'network only' version
+df_net <- df_prep %>% 
+  dplyr::select(question, answer, dplyr::starts_with("network_")) %>% 
+  dplyr::distinct()
+
+
+# Clear environment
+rm(list = c("ord", "df_prep", "df_net", "focal_site"))
+
+
 
 
 
