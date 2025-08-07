15f3b5ab9e6c2dc2aaa5a9f23a0e69e36b482393
njlyon0 <nicholasjlyon@gmail.com>
2025-08-07 11:05:40 -0400

    dev (viz): partial effort at a test graph

--- 03a_visualize-climate.R
+++ 03a_visualize-climate.R
@@ -14,14 +14,10 @@
 ## ----------------------------- ##
 
 # Load libraries
-librarian::shelf(tidyverse, supportR)
+librarian::shelf(tidyverse)
 
 # Clear environment
 rm(list = ls()); gc()
-
-# Load custom function(s)
-purrr::walk(.x = dir(path = file.path("tools")),
-            .f = ~ source(file.path("tools", .x)))
 
 ## ----------------------------- ##
 # Read in Data ----
@@ -34,12 +30,47 @@ comp_v1 <- read.csv(file = file.path("data", "02a_composite-scores.csv"))
 dplyr::glimpse(comp_v1)
 
 # Also read in summarized data
-sry_v1 <- read.csv(file = file.path("data", "02a_summarized-climate.csv"))
+res_v1 <- read.csv(file = file.path("data", "02a_summarized-climate.csv"))
 
 # Check structure
-dplyr::glimpse(sry_v1)
+dplyr::glimpse(res_v1)
+
+# Make 'network-wide' and 'site-specific' variants of both
+## Composites
+comp_net <- dplyr::filter(.data = comp_v1, site == "Network")
+comp_site <- dplyr::filter(.data = comp_v1, site != "Network")
+## General results
+res_net <- dplyr::filter(.data = res_v1, site == "Network")
+res_site <- dplyr::filter(.data = res_v1, site != "Network")
+
+## ----------------------------- ##
+# Activities ----
+## ----------------------------- ##
+
+# Identify preferred order
+ord <- res_net %>% 
+  dplyr::filter(question == "respondent_activities") %>% 
+  dplyr::arrange(-percent)
+
+view(ord)
+
+# Identify colors
 
 
+
+ggplot(data = dplyr::filter(res_net, question == "respondent_activities"), 
+       aes(x = percent, y = answer, fill = answer)) +
+  geom_bar(stat = "identity") +
+  facet_grid(. ~ site) +
+  theme_bw() +
+  theme(strip.text = element_text(size = 12),
+        legend.position = "none",
+        axis.title.y = element_blank())
+
+
+
+# Clear environment
+rm(list = setdiff(ls(), c("comp_net", "comp_site", "res_net", "res_site")))
 
 
 
