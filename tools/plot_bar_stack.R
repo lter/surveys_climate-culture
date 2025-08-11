61bb05d83cd55ece023a9b1cb56de2d26a78a5e2
njlyon0 <nicholasjlyon@gmail.com>
2025-08-11 14:17:18 -0400

    feat (tool): upgrade `plot_stack_bar` to not require manual colors

--- tools/plot_bar_stack.R
+++ tools/plot_bar_stack.R
@@ -4,13 +4,14 @@
 #' 
 #' @param df (data.frame-like) Table of survey responses containing (at least) the following columns: 'site', 'question', 'answer', and 'percent'
 #' @param focal_q (character) Question within the 'question' column of `df` for which a plot is desired
-#' @param answer_colors (named character) Vector of colors where the vector names correspond exactly to the answer text for the focal question
+#' @param answers (character) Vector of answers in the desired order in the stacked barplot
+#' @param colors (character) Vector of colors where the vector names correspond exactly to the answer text for the focal question
 #' 
 #' @return (ggplot) Stacked barplot for the desired question
 #' 
 #' @importFrom magrittr %>%
 #' 
-plot_bar_stack <- function(df = NULL, focal_q = NULL, answer_colors = NULL){
+plot_bar_stack <- function(df = NULL, focal_q = NULL, answers = NULL, colors = NULL){
   
   # Error checks for 'df'
   if(is.null(df) || "data.frame" %in% class(df) != TRUE || 
@@ -22,22 +23,25 @@ plot_bar_stack <- function(df = NULL, focal_q = NULL, answer_colors = NULL){
      focal_q %in% unique(df$question) != TRUE)
     stop("'focal_q' must be a character that exactly matches an entry in the 'question' column of 'df'")
   
-  # Error checks for 'answer_colors'
-  if(is.null(answer_colors) || is.null(names(answer_colors)) || is.character(answer_colors) != TRUE)
-    stop("'answer_colors' must be a _named_ character vector where names correspond to entries in the 'answer' column of 'df' and values correspond to fill colors for those answers")
+  # Error checks for 'answers'
+  if(is.null(answers) ||  is.character(answers) != TRUE)
+    stop("'answers' must be a character vector corresponding to entries in the 'answer' column of 'df'")
+  
+  # Error checks for 'colors'
+  if(is.null(colors) != TRUE & any(is.null(names(colors)) || is.character(colors) != TRUE))
+    stop("'colors' must be a _named_ character vector where names correspond to entries in the 'answer' column of 'df' and values correspond to fill colors for those colors")
   
   # Prepare the data for plotting
   df_v2 <- df %>% 
     # Filter to focal question
     dplyr::filter(question == focal_q) %>% 
     # Order answers as specified by user
-    dplyr::mutate(answer = factor(answer, levels = rev(names(answer_colors))))
+    dplyr::mutate(answer = factor(answer, levels = rev(answers)))
   
   # Generate the graph
   p <- ggplot(df_v2, aes(x = site, y = percent, fill = answer, color = "x")) +
     geom_bar(stat = "identity") +
     scale_color_manual(values = "#000") +
-    scale_fill_manual(values = ord) +
     labs(y = "Percent Responses") +
     guides(color = "none") +
     theme_bw() +
@@ -47,6 +51,12 @@ plot_bar_stack <- function(df = NULL, focal_q = NULL, answer_colors = NULL){
           axis.title.y = element_text(size = 12),
           axis.text.x = element_text(size = 12),
           axis.text.y = element_text(size = 10))
+  
+  # If colors is not NULL, add manual colors
+  if(is.null(colors) != TRUE){
+    p <- p +
+      scale_fill_manual(values = ord)
+  }
   
   # Return the graph
   return(p) }
