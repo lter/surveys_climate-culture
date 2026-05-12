ed3bb5f57971c07e4dfac46b0679702257e7d7f2
njlyon0 <nicholasjlyon@gmail.com>
2026-05-12 16:59:40 -0400

    feat: replace graph points with site names

--- sandbox/lyon_demo-ordinations.r
+++ sandbox/lyon_demo-ordinations.r
@@ -44,16 +44,9 @@ as.data.frame(pcoa_mod$vectors) %>%
   dplyr::select(Axis.1, Axis.2) %>% 
   dplyr::mutate(site = comp_v02$site) %>% 
   ggplot(., aes(x = Axis.1, y = Axis.2, fill = site)) +
-    geom_point(pch = 21, size = 2, alpha = 0.5) +
+    geom_text(aes(label = site)) +
     labs(x = paste0("PC1 (", round(pcoa_mod$values$Relative_eig[1] * 100, digits = 2), "%)"),
         y = paste0("PC2 (", round(pcoa_mod$values$Relative_eig[2] * 100, digits = 2), "%)")) +
-    scale_fill_manual(values = c("#780000", "#c1121f", "#003049", "#669bbc",
-    "#ffb703", "#fb8500", "#606c38", "#283618",
-    "#fff3b0", "#fb5607", "#ff006e", "#8338ec",
-    "#3a86ff", "#7f4f24", "#c2c5aa", "#a7c957",
-    "#6a994e", "#386641", "#caf0f8", "#00b4d8",
-    "#e9ff70", "#ff9770", "#ffd670", "#70d6ff",
-    "#ffb3c1", "#fff", "#000")) +
     supportR::theme_lyon()
 
 ggsave(file.path("graphs", "composite-ordination_2026-05-12_pcoa.png"), height = 7, width = 7, units = "in")
