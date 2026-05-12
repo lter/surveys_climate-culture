a379a9d8ea45c45a4a888b0ecf90f0d3687cae55
njlyon0 <nicholasjlyon@gmail.com>
2026-05-12 14:57:15 -0400

    feat: finish draft of ordination demo of C&C composite scores

--- sandbox/lyon_demo-ordinations.r
+++ sandbox/lyon_demo-ordinations.r
@@ -17,24 +17,33 @@ rm(list = ls()); gc()
 ## ----------------------------------- ##
 
 # Load data
-fine_v01 <- read.csv(file.path("data", "01a_processed-climate.csv"))
+comp_v01 <- read.csv(file.path("data", "02a_composite-scores.csv"))
 
 # Check structure
-dplyr::glimpse(fine_v01)
+dplyr::glimpse(comp_v01)
 
 # Do necessary prep
-fine_v02 <- fine_v01 %>% 
-    dplyr::select(site, dplyr::where(fn = ~ is.numeric(.) == TRUE)) %>% 
-    dplyr::select(-dplyr::starts_with("luq_")) %>% 
-    dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
-        .fns = ~ ifelse(nchar(.) == 0 | is.na(.), yes = 0, no = .)))
+comp_v02 <- comp_v01 %>% 
+    dplyr::select(site, climate_score_mean, dplyr::ends_with("_score"))
+
+# What's lost?
+supportR::diff_check(old = names(comp_v01), new = names(comp_v02))
 
 # Check structure
-dplyr::glimpse(fine_v02)
+dplyr::glimpse(comp_v02)
 
-# Create distance matrices
-fine_dist <- vegan::vegdist(x = fine_v02[-1], )
+# Compute distance matrix
+comp_dist <- vegan::vegdist(x = comp_v02[-1], method = "bray")
 
+# Perform PCoA / NMS
+pcoa_mod <- ape::pcoa(comp_dist)
+nms_mod <- vegan::metaMDS(comp_dist, autotransform = FALSE, expand = FALSE, k = 2, try = 50)
 
+# Create PCoA ordination (with optional agruments)
+supportR::ordination(mod = pcoa_mod, grps = comp_v02$site, 
+    lty = 2, col = "black")
+
+# Create NMS ordination
+supportR::ordination(mod = nms_mod, grps = comp_v02$site)
 
 # End ----
