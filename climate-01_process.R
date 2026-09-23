4301dc0693118a4df96bf2182e5630ae41ae3e91
njlyon0 <nicholasjlyon@gmail.com>
2026-09-23 14:14:02 -0400

    feat (clim): update process script, remove non-informative indents, and update in/output naming convention (per lter/scicomp#120)

--- climate-01_process.R
+++ climate-01_process.R
@@ -5,7 +5,7 @@
 ## Process raw survey responses into a format that is ready for summarization and visualization
 
 # Pre-requisites:
-## Download raw data and data key (can be done with `00_setup.R`)
+## Download raw data and data key (can be done with `drive-interactions / 00_download.R`)
 
 ## ----------------------------- ##
 # Housekeeping ----
@@ -25,7 +25,7 @@ source(file = file.path("-setup.R"))
 ## ----------------------------- ##
 
 # Identify file name
-clim_rawname <- "2024_LTER Climate_Survey_11_19_2024_11.16_de_identified.xlsx"
+clim_rawname <- "00_2024_LTER Climate_Survey_11_19_2024_11.16_de_identified.xlsx"
 
 # Read in the data
 clim_v1 <- readxl::read_excel(path = file.path("data", "raw", clim_rawname))
@@ -35,22 +35,22 @@ dplyr::glimpse(clim_v1)
 
 # Get this into the right format for `ltertools`
 clim_list <- list()
-clim_list[[clim_rawname]] <- clim_v1
+clim_list[[gsub("00_", "", clim_rawname)]] <- clim_v1
 
 ## ----------------------------- ##
 # Fix Column Names ----
 ## ----------------------------- ##
 
 # Read in data key
-key_obj <- read.csv(file = file.path("data", "climate24_data-key.csv")) %>% 
+key_obj <- readxl::read_excel(path = file.path("data", "raw", "00_climate24_data-key.xlsx")) %>% 
   ltertools::check_key(key = .)
 
 # Structure check
 dplyr::glimpse(key_obj)
 
 # Rename columns more informatively
-clim_v2 <- ltertools::standardize(focal_file = clim_rawname,
-                                  key = key_obj, df_list = clim_list)
+clim_v2 <- ltertools::standardize(focal_file = gsub("00_", "", clim_rawname),
+  key = key_obj, df_list = clim_list)
 
 # Check structure
 dplyr::glimpse(clim_v2)
@@ -64,8 +64,7 @@ clim_v3 <- clim_v2 %>%
   dplyr::filter(response_id != "Response ID") %>% 
   # Also make all NAs true NAs
   dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
-                              .fns = ~ ifelse(test = nchar(.) == 0,
-                                              yes = NA, no = .))) %>% 
+    .fns = ~ ifelse(test = nchar(.) == 0, yes = NA, no = .))) %>% 
   # And remove all columns that are entirely empty
   dplyr::select(-dplyr::where(fn = ~ all(is.na(.))))
 
@@ -85,56 +84,43 @@ clim_v4 <- clim_v3 %>%
   dplyr::mutate(
     ## Administrative duties
     activity_admin = ifelse(stringr::str_detect(string = respondent_activities,
-                                                pattern = "Administrative duties"),
-                            yes = 1, no = 0),
+      pattern = "Administrative duties"), yes = 1, no = 0),
     ## Education, outreach, and/or public engagement
     activity_education = ifelse(stringr::str_detect(string = respondent_activities,
-                                                    pattern = "Education, outreach, and/or public engagement"),
-                                yes = 1, no = 0),
+      pattern = "Education, outreach, and/or public engagement"), yes = 1, no = 0),
     ## Any type of field work
     activity_fieldwork_any = ifelse(stringr::str_detect(string = respondent_activities,
-                                                        pattern = "Field work"),
-                                    yes = 1, no = 0),
+      pattern = "Field work"), yes = 1, no = 0),
     ## Field work (land-based)
     activity_fieldwork_land = ifelse(stringr::str_detect(string = respondent_activities,
-                                                         pattern = "land-based"),
-                                     yes = 1, no = 0),
+      pattern = "land-based"), yes = 1, no = 0),
     ## Field work (small boats)
     activity_fieldwork_boat = ifelse(stringr::str_detect(string = respondent_activities,
-                                                         pattern = "small boats"),
-                                     yes = 1, no = 0),
+      pattern = "small boats"), yes = 1, no = 0),
     ## Field work (ship-based)
     activity_fieldwork_ship = ifelse(stringr::str_detect(string = respondent_activities,
-                                                         pattern = "ship-based"),
-                                     yes = 1, no = 0),
+      pattern = "ship-based"), yes = 1, no = 0),
     ## Lab work
     activity_lab = ifelse(stringr::str_detect(string = respondent_activities,
-                                              pattern = "Lab work"),
-                          yes = 1, no = 0),
+      pattern = "Lab work"), yes = 1, no = 0),
     ## Research
     activity_research = ifelse(stringr::str_detect(string = respondent_activities,
-                                                   pattern = "Research"),
-                               yes = 1, no = 0),
+      pattern = "Research"), yes = 1, no = 0),
     ## Modeling
     activity_model = ifelse(stringr::str_detect(string = respondent_activities,
-                                                pattern = "Modeling"),
-                            yes = 1, no = 0),
+      pattern = "Modeling"), yes = 1, no = 0),
     ## Information management
     activity_im = ifelse(stringr::str_detect(string = respondent_activities,
-                                             pattern = "Information management"),
-                         yes = 1, no = 0),
+      pattern = "Information management"), yes = 1, no = 0),
     ## In-person events
     activity_event_inperson = ifelse(stringr::str_detect(string = respondent_activities,
-                                                         pattern = "In-person events"),
-                                     yes = 1, no = 0),
+      pattern = "In-person events"), yes = 1, no = 0),
     ## Virtual events
     activity_event_virtual = ifelse(stringr::str_detect(string = respondent_activities,
-                                                        pattern = "Virtual events"),
-                                    yes = 1, no = 0),
+      pattern = "Virtual events"), yes = 1, no = 0),
     ## Synthesis
     activity_synthesis = ifelse(stringr::str_detect(string = respondent_activities,
-                                                    pattern = "Synthesis"),
-                                yes = 1, no = 0),
+      pattern = "Synthesis"), yes = 1, no = 0),
     .after = respondent_activities) %>% 
   # Rename the 'other' category column
   dplyr::rename(activity_other = respondent_activities_other)
@@ -242,12 +228,10 @@ sort(unique(clim_v5$site))
 clim_v6 <- clim_v5 %>% 
   # Resolve "Other" entry (just one response had this)
   dplyr::mutate(site = ifelse(site == "Other",
-                              # Response in 'site other' column: "pie and gce"
-                              yes = "Plum Island Ecosystems LTER (PIE)", 
-                              no = site)) %>% 
+    # Response in 'site other' column: "pie and gce"
+    yes = "Plum Island Ecosystems LTER (PIE)", no = site)) %>% 
   # Split into separate columns by parentheses
-  tidyr::separate_wider_delim(cols = site, delim = " (",
-                              names = c("site_name", "site")) %>% 
+  tidyr::separate_wider_delim(cols = site, delim = " (", names = c("site_name", "site")) %>% 
   # Remove trailing parentheses left over
   dplyr::mutate(site = gsub(pattern = "\\)", replacement = "", x = site)) %>% 
   # Relocate to start of data
@@ -267,8 +251,8 @@ dplyr::glimpse(clim_v6)
 clim_v7 <- clim_v6 %>% 
   # Antagonistic interactions
   dplyr::mutate(antagonistic_interactions_stage = ifelse(is.na(antagonistic_interactions_stage),
-                                                         yes = "No antagonistic interactions",
-                                                         no = antagonistic_interactions_stage))
+    yes = "No antagonistic interactions",
+    no = antagonistic_interactions_stage))
 
 # Check structure
 dplyr::glimpse(clim_v7)
@@ -282,6 +266,6 @@ clim_v99 <- clim_v7
 
 # Export locally
 write.csv(x = clim_v99, row.names = F, na = '',
-          file = file.path("data", "01a_processed-climate.csv"))
+  file = file.path("data", "climate-01_processed-data.csv"))
 
 # End ----
